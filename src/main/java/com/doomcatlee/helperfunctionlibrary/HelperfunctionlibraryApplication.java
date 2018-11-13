c82bdf7d148c824d71f2117a43c5cad5b106cc4d
Alex Lee <doomcat9167@gmail.com>
2018-11-13 10:21:27 -0800

    initilize

---
+++ src/main/java/com/doomcatlee/helperfunctionlibrary/HelperfunctionlibraryApplication.java
@@ -0,0 +1,12 @@
+package com.doomcatlee.helperfunctionlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelperfunctionlibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelperfunctionlibraryApplication.class, args);
+    }
+}
